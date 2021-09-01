f37862dd1637584c628ecf5713a3c46a42e2b4f6
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-01 21:34:08 +0300

    techpack: camera: import xiaomi changes

--- techpack/camera/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.h
+++ techpack/camera/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.h
@@ -63,8 +63,14 @@ int cam_sensor_util_request_gpio_table(
 int cam_sensor_util_init_gpio_pin_tbl(
 	struct cam_hw_soc_info *soc_info,
 	struct msm_camera_gpio_num_info **pgpio_num_info);
+
 int cam_sensor_core_power_up(struct cam_sensor_power_ctrl_t *ctrl,
 		struct cam_hw_soc_info *soc_info);
+
+#if IS_ENABLED(CONFIG_ISPV2_AL6021)
+int cam_sensor_core_power_up_extra(struct cam_sensor_power_ctrl_t *ctrl,
+		struct cam_hw_soc_info *soc_info);
+#endif
 
 int cam_sensor_util_power_down(struct cam_sensor_power_ctrl_t *ctrl,
 		struct cam_hw_soc_info *soc_info);
