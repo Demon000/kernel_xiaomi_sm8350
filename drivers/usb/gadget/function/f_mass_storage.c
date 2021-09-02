cdbdc539460a7fad27ab023ffbd7d2ab7f8db0a0
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-02 10:08:32 +0300

    usb: import xiaomi changes

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -2234,8 +2234,11 @@ reset:
 	}
 
 	common->running = 0;
-	if (!new_fsg || rc)
+	if (!new_fsg || rc) {
+		/* allow usb LPM after eps are disabled */
+		usb_gadget_autopm_put_async(common->gadget);
 		return rc;
+	}
 
 	common->fsg = new_fsg;
 	fsg = common->fsg;
@@ -2292,6 +2295,9 @@ reset:
 static int fsg_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 {
 	struct fsg_dev *fsg = fsg_from_func(f);
+
+	/* prevents usb LPM until thread runs to completion */
+	usb_gadget_autopm_get_async(fsg->common->gadget);
 
 	__raise_exception(fsg->common, FSG_STATE_CONFIG_CHANGE, fsg);
 	return USB_GADGET_DELAYED_STATUS;
