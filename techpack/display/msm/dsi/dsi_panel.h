ff8be20a611e57aa33096acede42e51a4721fbd4
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-09 20:50:03 +0300

    techpack: display: implement doze mode

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -248,6 +248,8 @@ struct dsi_panel {
 	enum dsi_panel_physical_type panel_type;
 
 	struct dsi_panel_ops panel_ops;
+
+	bool doze_enabled;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
