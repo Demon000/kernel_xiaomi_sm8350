f205ae84a590fceaae9181f0afebf641f0a34656
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-26 08:57:32 +0300

    techpack: display: protect setting fod doze status by allow_bl_update
    In the current implementation the doze status command might set the backlight
    of the panel before receiving a frame from the driver during the first commit.
    There is chance for garbage content to be shown onto the panel.
    This change imposes a condition to delay the fod hbm command until the
    first frame is received from the HW.

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -622,7 +622,7 @@ error:
 	return rc;
 }
 
-int dsi_panel_update_doze(struct dsi_panel *panel) {
+static int dsi_panel_update_doze(struct dsi_panel *panel) {
 	int rc = 0;
 
 	if (panel->fod_hbm_enabled) {
@@ -645,11 +645,27 @@ int dsi_panel_update_doze(struct dsi_panel *panel) {
 	return rc;
 }
 
-int dsi_panel_set_doze_status(struct dsi_panel *panel, bool status) {
+static int dsi_panel_set_doze_status(struct dsi_panel *panel, bool status) {
 	if (status == panel->doze_enabled)
 		return 0;
 
+	if (!panel->bl_config.allow_bl_update && status) {
+		panel->doze_requested = status;
+		return 0;
+	}
+
 	panel->doze_enabled = status;
+	panel->doze_requested = status;
+
+	return dsi_panel_update_doze(panel);
+}
+
+static int dsi_panel_apply_doze_status(struct dsi_panel *panel)
+{
+	if (panel->doze_requested == panel->doze_enabled)
+		return 0;
+
+	panel->doze_enabled = panel->doze_requested;
 
 	return dsi_panel_update_doze(panel);
 }
@@ -810,6 +826,10 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 		DSI_ERR("Backlight type(%d) not supported\n", bl->type);
 		rc = -ENOTSUPP;
 	}
+
+	rc = dsi_panel_apply_doze_status(panel);
+	if (rc)
+		DSI_ERR("[%s] unable to apply doze on, rc=%d\n", panel->name, rc);
 
 	bl->real_bl_level = bl_lvl;
 
@@ -3896,6 +3916,7 @@ struct dsi_panel *dsi_panel_get(struct device *parent,
 	panel->drm_panel.dev = &panel->mipi_device.dev;
 	panel->mipi_device.dev.of_node = of_node;
 	panel->doze_enabled = false;
+	panel->doze_requested = false;
 	panel->fod_ui = false;
 	panel->fod_hbm_enabled = false;
 	panel->fod_hbm_requested = false;
@@ -5090,6 +5111,7 @@ int dsi_panel_disable(struct dsi_panel *panel)
 	panel->panel_initialized = false;
 	panel->power_mode = SDE_MODE_DPMS_OFF;
 	panel->doze_enabled = false;
+	panel->doze_requested = false;
 
 	mutex_unlock(&panel->panel_lock);
 	return rc;
