2d6c6870c70e27bdb190cb43c8b309fc4edf3d04
Cosmin Tanislav <demonsingur@gmail.com>
2022-01-27 00:02:14 +0200

    power: reset: qcom-dload-mode: perform a warm nodump reboot only if in panic

--- drivers/power/reset/qcom-dload-mode.c
+++ drivers/power/reset/qcom-dload-mode.c
@@ -258,8 +258,7 @@ static int qcom_dload_reboot(struct notifier_block *this, unsigned long event,
 						     reboot_nb);
 
 	/* Clean shutdown, disable dump mode to allow normal restart */
-	if (!poweroff->in_panic)
-		set_download_mode(QCOM_DOWNLOAD_NODUMP);
+	set_download_mode(QCOM_DOWNLOAD_NODUMP);
 
 	if (cmd) {
 		if (!strcmp(cmd, "edl"))
@@ -268,7 +267,7 @@ static int qcom_dload_reboot(struct notifier_block *this, unsigned long event,
 			msm_enable_dump_mode(true);
 	}
 
-	if (current_download_mode != QCOM_DOWNLOAD_NODUMP)
+	if (poweroff->in_panic)
 		reboot_mode = REBOOT_WARM;
 
 	return NOTIFY_OK;
